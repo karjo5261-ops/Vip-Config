09808516d229b118b1dfcb5fd22037c7e3b96805
Pendidikan-anakislam <karjo5261@gmail.com>
2026-08-06 22:05:18 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = "cikuya-v1";
+const urlsToCache = ["/", "/index.html", "/musik4.mp3", "/icon-192.png", "/icon-512.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
