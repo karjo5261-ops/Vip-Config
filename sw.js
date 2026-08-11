0a3aee4c11e760258b053615a3b63bc9ecf6a083
Pendidikan-anakislam <karjo5261@gmail.com>
2026-08-11 13:42:20 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,32 @@
-const CACHE_NAME = "cikuya-v1";
-const urlsToCache = ["/", "/index.html", "/musik4.mp3", "/icon-192.png", "/icon-512.png"];
+const CACHE_NAME = "cikuya-v2"; // ganti v2 biar cache lama ke-reset
+const urlsToCache = [
+  "./", 
+  "./index.html", 
+  "./manifest.json",
+  "./anime-bg.png", 
+  "./musik4.mp3", 
+  "./icon-192.png", 
+  "./icon-512.png"
+  // "./qris.png" // tambahin kalo pake qris
+];
 
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+// Biar cache lama kehapus pas update
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
 });
